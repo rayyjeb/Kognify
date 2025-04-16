29bcbcf3fa5bbb4946f8399a7308d0f7560870f3
Rajab Shoukath Kunnath <rajabshoukathis@gmail.com>
2025-04-16 21:04:25 +0530

    refactor: streamline error handling in Agent component and optimize call disconnect logic

--- firebase/client.ts
+++ firebase/client.ts
@@ -4,13 +4,13 @@ import { getApp, getApps, initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID!,
+  apiKey: "AIzaSyBDD9IA1kFxEE1ZDfXl4sPjkDnY6-Nc8Og",
+  authDomain: "kognify-4cc8f.firebaseapp.com",
+  projectId: "kognify-4cc8f",
+  storageBucket: "kognify-4cc8f.firebasestorage.app",
+  messagingSenderId: "1074319355158",
+  appId: "1:1074319355158:web:7bc84f1dfd264bd5243437",
+  measurementId: "G-4SVVBX5GEM"
 };
 
 // Initialize Firebase
